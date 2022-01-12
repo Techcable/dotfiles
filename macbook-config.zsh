8762743850a3e62b6d35dcd9337a99c80450ae6a
Techcable <Techcable@techcable.net>
2022-01-11 21:19:34 -0700

    Detect homebrew kegs: python@3.10 and lua@5.3
    These binaries are put last in the $PATH,
    so that `python` resovles to standard python but `python3.10` resolves
    to the new version. Likewise with `lua` and `lua@5.3`

--- macbook-config.zsh
+++ macbook-config.zsh
@@ -15,6 +15,31 @@ extend_path ~/bin
 # TODO: I really don't like hardcoding these
 extend_path ~/.rustup/toolchains/nightly-aarch64-apple-darwin/bin
 
+# Some homebrew things are "keg-only" meaning they are not on the path by default
+#
+# Usually these are alternative versions of the main package.
+# Particular examples are lua@5.3 and python@3.10
+#
+# We want these on the path, but we want them at the end (lower precedence)
+# so they don't conflict with existing versions
+#
+# Use the extend_path builtin to add it to the end (but only if the keg exists)
+function detect_keg() {
+    local keg_prefix="/opt/homebrew/opt";
+    if [[ -d "${keg_prefix}/$1/bin" ]]; then
+        # echo "Detected keg $1";
+        extend_path "${keg_prefix}/$1/bin"
+    fi
+}
+detect_keg "python@3.10"
+detect_keg "lua@5.3"
+
+# Mac has no LDD command
+#
+# See here: https://discussions.apple.com/thread/309193 for suggestion
+# Also "Rosetta stone for Unixes"
+alias ldd="echo 'Using otool -L' && otool -L"
+
 # NOTE: Prefix with 'py' to indicate we are in xonsh
 # We really should be prefixing with 'xonsh', but 'py' is shorter
 # It's not really ambiguous, since this is really the python-prompt (for all
