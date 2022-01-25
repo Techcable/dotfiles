b16c3540578a369603e979ba67b9300fe446216f
Techcable <Techcable@techcable.net>
2022-01-24 20:53:33 -0700

    Add a 'panrender' script
    Renders things with pandoc then opens them in the browser ;)

---
+++ scripts/panrender.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import os
+import sys
+import tempfile
+import shutil
+from subprocess import run, CalledProcessError, DEVNULL
+from pathlib import Path
+
+import click
+
+if sys.platform == "darwin":
+    OPEN_COMMAND = "open"
+elif sys.platform == "linux":
+    OPEN_COMMAND = os.getenv("BROWSER", "xdg-open")
+else:
+    # TODO: Should we use the builtin browser module or something?
+    raise NotImplementedError(f"Unknown platform (how would I open things?): {sys.platform}") 
+
+# Default file formats based on extensions
+#
+# Implicitly overrides the pandoc defaults
+DEFAULT_FILE_FORMATS = {
+    'md': 'gfm', # Render markdown as github-flavored by default
+}
+
+@click.command()
+@click.option('--verbose', '-v', help="Be verbose internally", count=True)
+@click.option('--quiet', '-q', is_flag=True, help="Suppress pandoc outputs (mainly warnings)")
+@click.option('--format', '-f', 'input_format', help="The format to pass to pandoc, by default inferred from the filename")
+@click.argument('input_file', type=click.Path(path_type=Path))
+def panrender(input_file, input_format=None, verbose=False, quiet=False):
+    """A simple wrapper around `pandoc` that renders the input HTML,
+    then opens it in the browser.
+
+    Supports every feature pandoc does, since it's just a thin wrapper around it"""
+    handle, temp_file = tempfile.mkstemp(suffix=".html", text=True)
+    os.close(handle)
+    temp_file = Path(temp_file)
+    assert temp_file.suffix == ".html", f"Invalid temp file: {temp_path}"
+    if input_format is None:
+        # try and auto-detect
+        file_extension = input_file.suffix.lstrip('.')
+        try:
+            input_format = DEFAULT_FILE_FORMATS[file_extension]
+        except KeyError:
+            assert input_format is None
+        else:
+            if verbose >= 2:
+                print("Inferred input format: {input_format!r}")
+    pandoc_args = ['pandoc', str(input_file)]
+    if input_format is not None:
+        pandoc_args.extend(('--from', str(input_format)))
+    assert temp_file is not None
+    pandoc_args.extend(('-o', str(temp_file)))
+    if verbose >= 1:
+        pandoc_args.append('--verbose')
+    if quiet:
+        pandoc_args.append('--quiet')
+    else:
+        print(f"Rendering {input_file} to {temp_file}")
+    try:
+        run(pandoc_args, stdin=DEVNULL, check=True)
+    except CalledProcessError:
+        raise click.ClickException(f"Failed to render {input_file} w/ pandoc")
+    if not quiet:
+       print(f"Opening rendered {input_file.name} in browser")
+    try:
+        run([OPEN_COMMAND, str(temp_file)], check=True)
+    except CalledProcessError:
+        raise click.ClickException(f"Failed to open browser w/ `{OPEN_COMMAND}`")
+
+if __name__ == "__main__":
+    panrender()
