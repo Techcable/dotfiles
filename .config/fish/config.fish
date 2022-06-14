d020b5138f9bdabd72c9dc57e8d01c337466ec81
Techcable <Techcable@techcable.net>
2022-06-13 18:12:08 -0700

    Fix detecting MACHINE_NAME in config.fish
    Relies on Lua for parsing ^_^

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,8 @@
 #
 # I should look into it in case xonsh falls flat :)
 
-set machine_name (cat ~/.config.zsh | grep 'export MACHINE_NAME' | sed -E 's/export MACHINE_NAME="(.*)"/\1/')
+# TODO: Is this ever used?
+set machine_name (rg 'export\(.*MACHINE_NAME' ~/.shell-config.py | env LUA_INIT="export = function(a, b) print(b) end" lua)
 
 if test $machine_name = ""
    echo "WARNING: Empty machine name"
@@ -23,8 +24,6 @@ end
 if test -d "$techcable_machine_completions"
     set fish_complete_path $fish_complete_path $techcable_machine_completions
 end
-
-set machine_name (cat ~/.config.zsh | grep 'export MACHINE_NAME' | sed -E 's/export MACHINE_NAME="(.*)"/\1/')
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
