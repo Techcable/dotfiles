8c918d43a8002237a0bb3dc0bdb4ad90c6e0e622
Techcable <Techcable@techcable.net>
2022-06-18 05:10:09 -0700

    Disable "automatic" shell integration for kitty
    Enable shell integration for fish (but not for zsh fallback)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -67,4 +67,10 @@ if status is-interactive
     # Thanks to @faho for pointing this out on Gitter 
     bind \e\[A history-prefix-search-backward
     bind \e\[B history-prefix-search-forward
+
+    if set -q KITTY_INSTALLATION_DIR
+        set --global KITTY_SHELL_INTEGRATION enabled
+        source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+        set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+    end
 end
