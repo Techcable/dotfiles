34d5b47e5cc45d79168bf2702342ecbb651ac755
Techcable <Techcable@techcable.net>
2023-01-18 23:15:13 -0700

    Merge with changes on my old arch laptop

--- .config/ptpython/config.py
+++ .config/ptpython/config.py
@@ -2,7 +2,11 @@ __all__ = ["configure"]
 
 
 def configure(repl):
-    # default to vi mode
+    # With persistent history, there's really
+    # no need to confirm exit....
+    repl.confirm_exit = False
+
+    # Begin in VI mode (because it's the best)
     repl.vi_mode = True
 
     # enable mouse support
@@ -14,6 +18,8 @@ def configure(repl):
     # NOTE: Be careful because some colorschemes are hard
     # to see in the terminal!
     repl.use_code_colorscheme("monokai")
+    repl.min_brightness = .30
+    repl.color_depth = "DEPTH_24_BIT" # Truecolor
 
     # If output is very large, use a pager
     repl.enable_pager = True
