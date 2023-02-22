bc16d6ca5573ff1c05fdf3a254d21f3debba9156
Techcable <Techcable@techcable.net>
2023-02-22 13:15:00 -0700

    macbook: Explicitly set PIPX_DEFAULT_PYTHON
    Avoids minor version mistakes

--- shellrc/macbook-config.py
+++ shellrc/macbook-config.py
@@ -1,13 +1,21 @@
 # Configuration for my 2021 Macbook Pro
+from __future__ import annotations
+
+import operator
 import re
+from dataclasses import dataclass
 from pathlib import Path
 from subprocess import PIPE, CalledProcessError, run
-from typing import TYPE_CHECKING, Optional, Union
+from typing import TYPE_CHECKING, ClassVar, Iterator, Optional, Union
 
 from translate.cache import Cache, CachedValue, RehashFilesChanged
 
 if TYPE_CHECKING:
+    from typing import overload
+
     from translate.config_api import *
+
+HOMEBREW_ROOT = Path("/opt/homebrew")
 
 CACHE = Cache.open("macbook-config")
 
@@ -82,8 +90,97 @@ def setup_app_alias(app_name: str):
 for app_name in ["Keybase", "Sublime Text", "Texifier"]:
     setup_app_alias(app_name)
 
+
+class PythonVersion(tuple[int, ...]):
+    if TYPE_CHECKING:
+
+        @overload
+        def __init__(self, s: str) -> None:
+            pass
+
+        @overload
+        def __init__(self, *args: int) -> None:
+            pass
+
+    def __new__(cls, *args) -> PythonVersion:
+        match args:
+            case [str(val)]:
+                return super().__new__(cls, map(int, val.split(".")))
+            case tuple(_):
+                assert all(isinstance(val, int) for val in args)
+                assert len(args) >= 1
+                return super().__new__(cls, args)
+            case _:
+                raise TypeError(type(args))
+
+    def __repr__(self) -> str:
+        return f"PythonVersion({str(self)!r})"
+
+    def __str__(self) -> str:
+        return ".".join(map(str, self))
+
+
+@dataclass(frozen=True)
+class PythonInterpreter:
+    brew_name: str
+    version: PythonVersion
+    home: Path
+
+    def __post_init__(self):
+        assert self.version[0] == 3, "Only python 3 is supported"
+        assert self.brew_name.startswith(PythonInterpreter.HOMEBREW_NAME_PREFIX)
+        if not self.binary.is_file():
+            raise FileNotFoundError(f"Missing binary for {self}")
+
+    @property
+    def binary(self) -> Path:
+        return self.home / "bin" / f"python{self.version}"
+
+    @property
+    def source(self) -> str:
+        return "homebrew"
+
+    def __str__(self):
+        return f"{self.source} python{self.version} @ {self.home}"
+
+    HOMEBREW_NAME_PREFIX: ClassVar[str] = "python@"
+
+    @staticmethod
+    def homebrew_interpreters() -> Iterator[PythonInterpreter]:
+        """Detect (homebrew) python interpreters"""
+        name_prefix = PythonInterpreter.HOMEBREW_NAME_PREFIX
+        for python_home in (HOMEBREW_ROOT / "opt").glob("python@3.*"):
+            assert (python_home / "bin").is_dir()
+            name = python_home.name
+            assert name != name_prefix, "Should only return specifics!"
+            assert name.startswith(name_prefix)
+            ver = PythonVersion(name.removeprefix(name_prefix))
+            yield PythonInterpreter(
+                brew_name=python_home.name, version=ver, home=python_home
+            )
+
+    @staticmethod
+    def latest_homebrew_interpreter() -> PythonInterpreter:
+        """Return the latest homebrew interpreter"""
+        # Throws IndexError if none installed
+        return sorted(
+            PythonInterpreter.homebrew_interpreters(),
+            key=operator.attrgetter("version"),
+        )[-1]
+
+
 # pipx
 extend_path("~/.local/bin")  # path for pipx
+
+# Override pipx default python
+#
+# Otherwise it will hardcode against a specific minor version like:
+# /opt/homebrew/Cellar/python@3.11/3.11.2/Frameworks/Python.framework/Versions/3.11/bin/python3.11
+#
+# This will cause problems when minor version upgrades
+export(
+    "PIPX_DEFAULT_PYTHON", str(PythonInterpreter.latest_homebrew_interpreter().binary)
+)
 
 # $GOBIN (used for go install)
 go_bin = Path.home() / "go/bin"
