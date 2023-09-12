c4095aeab78668c10b4a5bf4497c3295cb63712a
Techcable <Techcable@techcable.net>
2023-09-12 13:36:14 -0700

    Port raspberrypi4_2022 from Python -> fish
    NOTE: There's almost nothing to do here.
    Was the config file unfinished?

---
+++ machines/shellrc/raspberrypi4_2022.fish
@@ -0,0 +1,5 @@
+# Configuration for my 2022 Raspberry Pi 4
+
+set -gx MACHINE_NAME "raspberrypi4-2022"
+
+# TODO: Is there more to do here?
--- machines/shellrc/raspberrypi4_2022.py
+++
@@ -1,7 +0,0 @@
-# Configuration for my 2022 Raspberry Pi 4
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from dotfiles.translate_shell.config_api import *
-
-export("MACHINE_NAME", "raspberrypi4-2022")
