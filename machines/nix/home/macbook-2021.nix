3bcb617ed7bf1a11829d9091e2080045ce25b872
Techcable <git@techcable.net>
2023-10-09 01:54:42 -0700

    macbook-2021: Drop nix package 'jupyter'
    Wasn't working right because of all those nix paths.
    Go back to old way that used pipx.
    
    Eventually want to use https://github.com/tweag/jupyter-nix
    if at all possible.

--- machines/nix/home/macbook-2021.nix
+++ machines/nix/home/macbook-2021.nix
@@ -3,10 +3,14 @@
     # GNU Octave (Matlab)
     #
     # We want the 'full' package for QT support.
-    # Unfortunately, it isn't in the cache right now :(
+    # Unfortunately, this package isn't in the Nix cache right now :(
+    #
+    # For my Math 412 class (Fall 2023), I started using this with Jupyter notebooks
+    # I use this with jupyterlab installed via `pipx`
+    # Using the PyPi package 'octave_kernel' I add support via `pipx inject`
+    #
+    # Unfortunately https://github.com/tweag/jupyenv doesn't work with macs yet
+    # It also doesn't offer an octave kernel :(
     octaveFull
-
-    # Scientific computing
-    jupyter-all
   ];
 }
