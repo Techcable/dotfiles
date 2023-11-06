Techcable/dotfiles

commit 0f460a8ae0551e03367bd659c142a46c78ce6db6

Port archlinux-lenovo-2017 from Python-> fish
Once again I will note the Python isn't really a 'DSL' in the traditional sense.

machines/shellrc/archlinux_lenovo_2017.fish
@@ -1,0 +1,28 @@
+# Configuration for my 2017 Arch Linux Lenovo Laptop
+
+
+set -gx MACHINE_NAME macbook-2021
+set -gx MACHINE_NAME_SHORT macbook
+set -gx MACHINE_PREFIX_COLOR $(set_color --bold "cyan")
+
+set -gx BROWSER "/usr/bin/firefox"
+
+fish_add_path --global "$HOME/.yarn/bin"
+fish_add_path --global "$HOME/go/bin"
+
+# Where pip puts its bin files
+fish_add_path "$HOME/.local/bin"
+
+# TODO: I really don't like hardcoding this
+fish_add_path "$HOME/.gem/ruby/2.5.0/bin"
+
+# TODO: Remove this and rely on ~/.cargo bin (See commit ee1052b64946896)
+fish_add_path "$HOME/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin"
+
+# aurutils
+set -gx AUR_DEST "$HOME/git/aur"
+set -gx AUR_PAGER "ranger"
+# seems like a good idea....
+set -gx AUR_CONFIRM_PAGER 1
+# this is magic...
+set -gx AUR_SYNC_USE_NINJA 1

machines/shellrc/archlinux_lenovo_2017.py
@@ -1,41 +1,0 @@
-# Configuration for my 2017 Arch Linux Lenovo Laptop
-from pathlib import Path
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from translate.config_api import *
-
-if not PLATFORM.is_desktop():
-    warning("Not on a desktop! (TODO: Refine check)")
-
-export("MACHINE_NAME", "archlinux-lenovo-2017")
-export("MACHINE_NAME_SHORT", "archtop")
-export("MACHINE_PREFIX_COLOR", set_color("cyan", bold=True))
-export("BROWSER", "/usr/bin/firefox")
-
-extend_path("~/.yarn/bin")
-extend_path("~/go/bin")
-
-# Where pip puts its bin files
-extend_path("~/.local/bin")
-# TODO: I really don't like hardcoding these
-extend_path("~/.gem/ruby/2.5.0/bin")
-# TODO: Remove this and rely on ~/.cargo bin (See commit ee1052b64946896)
-extend_path("~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/")
-
-# NOTE: Prefix with 'py' to indicate we are in xonsh
-# We really should be prefixing with 'xonsh', but 'py' is shorter
-# It's not really ambiguous, since this is really the python-prompt (for all
-# intents and purposes)
-# I'm not going to confuse with the regular python interpreter (python3) cause i'll
-# know its a shell
-export("XONSH_PREFIX", "py")
-export("XONSH_PREFIX_COLOR", "yellow")
-
-# aurutils
-export("AUR_DEST", Path.home() / "git/aur")
-export("AUR_PAGER", "ranger")
-# seems like a good idea....
-export("AUR_CONFIRM_PAGER", "1")
-# this is magic...
-export("AUR_SYNC_USE_NINJA", "1")

commit e47ffd541d55bc2538ca7c6ecb50a8dd7a236951

Correct machine name for archlinux-lenovo-2017
Was incorrectly calling it 'macbook'

This was originally broken in commit 0f460a8ae0

machines/shellrc/archlinux_lenovo_2017.fish
@@ -1,8 +1,7 @@
 # Configuration for my 2017 Arch Linux Lenovo Laptop
 
-
-set -gx MACHINE_NAME macbook-2021
-set -gx MACHINE_NAME_SHORT macbook
+set -gx MACHINE_NAME archlinux-lenovo-2017
+set -gx MACHINE_NAME_SHORT archtop
 set -gx MACHINE_PREFIX_COLOR $(set_color --bold "cyan")
 
 set -gx BROWSER "/usr/bin/firefox"