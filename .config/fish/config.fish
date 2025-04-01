256fa081fa1e0947f26fa2866545414bed441e61
Techcable <git@techcable.net>
2025-03-31 18:34:35 -0700

    config.fish: Default to vim keybindings
    I'm so happy I found this just today!
    Idind't realize it was builtin to the shell
    and thought I would have to use something like warp.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -114,6 +114,9 @@ begin
     end
 end
 
+# Default to vim keybindings
+set -g fish_key_bindings fish_vi_key_bindings
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
