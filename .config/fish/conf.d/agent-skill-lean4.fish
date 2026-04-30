e61e4f4d081ca7ffb0c0719de2a0753bd1936ccc
Techcable <git@techcable.net>
2026-04-30 12:46:49 -0700

    Configure lean4 skill env vars
    Using this skill is required for my math496T class.

---
+++ .config/fish/conf.d/agent-skill-lean4.fish
@@ -0,0 +1,5 @@
+# https://github.com/cameronfreer/lean4-skills/blob/main/INSTALLATION.md#environment-bootstrap-all-hosts
+set -gx LEAN4_PLUGIN_ROOT $HOME/git/lang.lean/lean4-skills/plugins/lean4
+set -gx LEAN4_SCRIPTS $LEAN4_PLUGIN_ROOT/lib/scripts
+set -gx LEAN4_REFS $LEAN4_PLUGIN_ROOT/skills/lean4/references
+
