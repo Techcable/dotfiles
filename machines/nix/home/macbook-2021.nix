3e574e99c8aa9feecfa5830e596eb7226fe17904
Techcable <git@techcable.net>
2026-05-26 21:21:45 -0700

    macbook: Add cross-compiler for x86_64 linux
    Needed for my work at indexable.

--- machines/nix/home/macbook-2021.nix
+++ machines/nix/home/macbook-2021.nix
@@ -4,5 +4,12 @@
 
     # needed for codex
     mgrep
+
+    ## Cross-Compilation
+
+    # compilers & bintools for x86_64-unknown-linux-musl, appropriately prefixed
+    pkgs.pkgsCross.musl64.stdenv.cc
+    # Likewise for x86_86-unknown-linux-gnu
+    pkgs.pkgsCross.gnu64.stdenv.cc
   ];
 }
