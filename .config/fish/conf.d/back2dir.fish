be72c1683db8d2d8f7a5b85fcf105cf979ed98a3
Techcable <git@techcable.net>
2026-05-28 01:30:45 -0700

    back2dir: Allow ignoring specific directories
    This is a workaround for the slowness of `direnv` in the indexable monorepo.

--- .config/fish/conf.d/back2dir.fish
+++ .config/fish/conf.d/back2dir.fish
@@ -7,16 +7,19 @@
 # TODO: Is using universal variables (as opposed to a dedicated file)
 # going to slow things down or hurt SSD?
 
-# Right before the first pompt is shown, we go back to the last directory
+# Right before the first prompt is shown, we go back to the last directory
 #
 # As far as I can tell fish has no equivalent to @events.on_post_init
+#
 function __back2dir_on_init --on-event fish_prompt
     if test "$PWD" = "$HOME" -a -n "$techcable_back2dir_last_dir"
-        if test -d $techcable_back2dir_last_dir
+        if contains -- "$techcable_back2dir_last_dir" $techcable_back2dir_ignored_dirs
+            echo (set_color --bold)NOTE(set_color normal): Directory (set_color -u white)$techcable_back2dir_last_dir$(set_color normal) is marked as ignored by $(set_color --bold)back2dir$(set_color normal), starting in '$HOME' instead
+        else if test -d $techcable_back2dir_last_dir
             cd $techcable_back2dir_last_dir
         else
             echo (set_color --bold yellow)WARNING(set_color brblack)\[back2dir\](set_color normal): Previous directory (set_color -u white)$techcable_back2dir_last_dir$(set_color normal) does not exist
-            # Reset last path to home to avoid warning?
+            # Reset last path to home to avoid warning
             set -U techcable_back2dir_last_dir $HOME
         end
     end
